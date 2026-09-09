8c8692df448208694df3d24a77717df6b989141c
Zezo-wav <abdulazizattass5@gmail.com>
2026-09-09 18:54:44 -0400

    Update sketch.js

--- week1/sketch.js
+++ week1/sketch.js
@@ -33,7 +33,7 @@ function pickQuote() {
 }
 
 function draw() {
-  background(255, 20, 250); // set the background color
+  background(717171); // set the background color
   drawQuote();  // draw the quote on screen
 }
 
