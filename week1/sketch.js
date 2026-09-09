6aa982d96ac79af1d3266efb6ecacd0f0aa223a4
Zezo-wav <abdulazizattass5@gmail.com>
2026-09-09 18:58:46 -0400

    Update sketch.js

--- week1/sketch.js
+++ week1/sketch.js
@@ -21,7 +21,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   // Seed with millis() so each page load gives a different quote.
   randomSeed(millis());
-  textSize(32);
+  textSize(19);
   fill(10, 255, 10);
   // calls the function to pick a quote
   pickQuote();
